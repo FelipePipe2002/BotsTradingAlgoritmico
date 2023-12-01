901eca707e8968541a420ea16046c5cbe6988917
FelipePipe2002 <64708520+FelipePipe2002@users.noreply.github.com>
2023-12-01 08:59:48 -0300

    Se agrego las señales de entrada y salida Hammer y Deadman

--- Estrategia.py
+++ Estrategia.py
@@ -14,8 +14,9 @@ class Strategy(bt.Strategy):
     params = (
         ("bollinger_period", 20),
         ("bollinger_dev", 2),
-        ("short_sma_period", 10),
+        ("short_sma_period", 15),
         ("long_sma_period", 60),
+        ("stop_loss_percent", 0.3),
     )
 
     def log(self, txt, dt=None):
@@ -55,6 +56,36 @@ class Strategy(bt.Strategy):
         if not trade.isclosed:
             return
 
+    def bad_days(self):
+
+        returns = [self.dataclose[i] - self.dataclose[i - 1] for i in range(-1, -4, -1)]
+        return all(return_ < 0 for return_ in returns)
+    
+    def good_days(self):
+        returns = [self.dataclose[i] - self.dataclose[i - 1] for i in range(-1, -4, -1)]
+        return all(return_ > 0 for return_ in returns)
+
+    def is_hammer(self):
+        # Check if the current candle is a hammer
+        close = self.dataclose[0]
+        open_ = self.datas[0].open[0]
+        high = self.datas[0].high[0]
+        low = self.datas[0].low[0]
+
+        # Conditions for a bullish hammer
+        return close > open_ and self.bad_days()
+
+    def is_deadman(self):
+        # Check if the current candle is a hammer
+        close = self.dataclose[0]
+        open_ = self.datas[0].open[0]
+        high = self.datas[0].high[0]
+        low = self.datas[0].low[0]
+
+        # Conditions for a bullish hammer
+        return close < open_ and self.good_days()
+
+
     def next(self):
         if self.order:
             return
@@ -63,17 +94,19 @@ class Strategy(bt.Strategy):
             # Buy when closing price is below the lower Bollinger Band
             # and short-term SMA is above long-term SMA
             if (self.dataclose[0] < self.bollinger.lines.bot[0] and
-                    self.short_sma > self.long_sma and
-                    self.short_sma > self.long_sma[-1]):
-                self.order = self.buy()
+                self.short_sma > self.long_sma and self.short_sma > self.long_sma[-1]
+                ) or self.is_hammer():
+                size = int(self.broker.get_cash() * .9 / self.dataclose)
+
+                self.order = self.buy(size=size)
 
         else:
             # Sell when closing price is above the upper Bollinger Band
             # and short-term SMA is below long-term SMA
             if (self.dataclose[0] > self.bollinger.lines.top[0] and
-                    self.short_sma < self.long_sma and
-                    self.short_sma < self.long_sma[-1]):
-                self.order = self.sell()
+                self.short_sma < self.long_sma and self.short_sma < self.long_sma[-1]
+                ) or self.is_deadman():
+                self.order = self.sell(size=self.position.size)
 
 if __name__ == '__main__':
     # Create a cerebro entity
@@ -91,7 +124,7 @@ if __name__ == '__main__':
     data = bt.feeds.YahooFinanceCSVData(
         dataname=datapath,
         # Do not pass values before this date
-        fromdate=datetime.datetime(1995, 1, 1),
+        fromdate=datetime.datetime(1996, 1, 1),
         # Do not pass values before this date
         todate=datetime.datetime(2014, 12, 31),
         # Do not pass values after this date
@@ -101,23 +134,22 @@ if __name__ == '__main__':
     cerebro.adddata(data)
 
     # Set our desired cash start
-    cerebro.broker.setcash(1000.0)
-
-    # Add a FixedSize sizer according to the stake
-    cerebro.addsizer(bt.sizers.FixedSize, stake=10)
+    cerebro.broker.setcash(100000.0)
     
 
     # Set the commission
-    cerebro.broker.setcommission(commission=0.0)
+    cerebro.broker.setcommission(commission=0.0001)
 
     # Print out the starting conditions
-    print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
+    print('Start Portfolio Value: %.2f, Cash: %.2f' % (cerebro.broker.getvalue(), cerebro.broker.get_cash()))
 
     # Run over everything
     cerebro.run()
 
-    # Print out the final result
-    print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
+    # Print out the final result with both portfolio value and cash
+    print('Final Portfolio Value: %.2f, Cash: %.2f' % (cerebro.broker.getvalue(), cerebro.broker.get_cash()))
+
 
     # Plot the result
-    #cerebro.plot()
+    cerebro.plot()
+    
